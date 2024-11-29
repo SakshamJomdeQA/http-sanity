53c1ce7eec282aa69bb14abff806ed151707acca
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-11-29 11:47:20 +0530

    add more methods

--- pages/api/api-route.js
+++ pages/api/api-route.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const responseJson = {method: req.method};
 
-  if (req.method === "POST") {
+  if (req.method === "POST" || req.method === "PATCH" || req.method === "PUT") {
     const body = req.body;
     if (body && body.bodyIdentifier === 'BODY_IDENTIFIER') {
       responseJson.body = 'VALID';
